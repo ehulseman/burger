80fae2d5ab0e294b830f0707f0f27a28d218c6f8
Elizah Hulseman <ehulseman@gmail.com>
2018-01-11 21:38:14 -0500

    initial commit devour and create new burger not working but displaying burgers from database is working correctly

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sausage Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
